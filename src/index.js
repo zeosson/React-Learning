d0ca804da35500a7901ce46c6ce671872a8ea71e
Mario A Gonzalez <zeosson@gmail.com>
2019-05-24 23:32:03 -0500

    Learned that you can call jsx with props.chidren to display it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker'
-import CommentDetail from './CommentDetail'
-const App = ()  => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today @ 4:15PM" picture={faker.image.avatar()} comment="A  qp"/>
-            <CommentDetail author="Alex" timeAgo="Yesterday @ 4:15PM" picture={faker.image.avatar()} comment="w"/>
-            <CommentDetail author="Jane" timeAgo="May 4 @ 4:15PM" picture={faker.image.avatar()} comment="Nice"/>
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
         </div>
-    );
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today @ 4:15PM"
+          picture={faker.image.avatar()}
+          comment="A  qp"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday @ 4:15PM"
+          picture={faker.image.avatar()}
+          comment="w"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="May 4 @ 4:15PM"
+          picture={faker.image.avatar()}
+          comment="Nice"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
